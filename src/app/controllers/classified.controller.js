ff55430a0a7763eda68774a367f39faeb14f9a20
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-03 16:13:06 +0300

    Add new classified item

--- src/app/controllers/classified.controller.js
+++ src/app/controllers/classified.controller.js
@@ -1,4 +1,9 @@
-function ClassifiedController($scope, $http, $mdSidenav, classifiedFactory) {
+function ClassifiedController($scope, $http, $mdSidenav, $mdToast, classifiedFactory) {
+	var oFakeContact = {
+		name: "Andrey Savelev",
+		phone: "(555) 555-55-55",
+		email: "savelevcorr@gmail.com"
+	};
 
 	classifiedFactory
 		.getClassifieds()
@@ -11,6 +16,25 @@ function ClassifiedController($scope, $http, $mdSidenav, classifiedFactory) {
 
 	$scope.toggleSidenav = function () {
 		$mdSidenav('leftSidenav').toggle();
+	};
+
+	$scope.saveClassified = function (classifiedItem) {
+		if (classifiedItem) {
+			classifiedItem.contact = oFakeContact;
+			$scope.classifieds.push(classifiedItem);
+
+			$mdToast.show(
+				$mdToast.simple()
+					.content('Classified ' + classifiedItem.title + ' saved!')
+					.position('top, right')
+					.hideDelay(3000)
+			);
+
+			// Clear form fields
+			$scope.classified = {};
+		}
+
+		$scope.toggleSidenav();
 	};
 }
 
