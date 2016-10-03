42534455074be98cd347580fd3e30def01c8db4b
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-03 16:14:07 +0300

    create angular config

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyB7F5MQ2hZZP4AvuigMmZcZB1xBKLWY-Uo",
+	authDomain: "classifieds-f3ac4.firebaseapp.com",
+	databaseURL: "https://classifieds-f3ac4.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "538924886704"
+};
+
+module.exports = config;
+
+//firebase.initializeApp(config);
