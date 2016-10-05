efa5df03ee130eb1847a02e8637b1642b4c7e5ab
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-05 14:24:45 +0300

    set up ui-routing

--- src/app/config/ng.config.js
+++ src/app/config/ng.config.js
@@ -1,7 +1,15 @@
-function materialConfig($mdThemingProvider) {
+function materialConfig($mdThemingProvider, $stateProvider) {
 	$mdThemingProvider.theme('default')
 					.primaryPalette('teal')
 					.accentPalette('orange');
+
+	// Routing
+	$stateProvider
+		.state('classifieds', {
+			url: '/classifieds',
+			template: require('../templates/classifieds.tpl.html'),
+			controller: 'ClassifiedController as vm'
+		})
 }
 
 module.exports = materialConfig;
