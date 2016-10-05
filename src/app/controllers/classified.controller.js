99cbd2de65bc18ce83bbc3e301653dbe5010c576
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-05 14:26:08 +0300

    refactoring controller to vm.*

--- src/app/controllers/classified.controller.js
+++ src/app/controllers/classified.controller.js
@@ -1,6 +1,16 @@
 var _ = require('lodash');
 
 function ClassifiedController($scope, $http, $mdSidenav, $mdToast, classifiedFactory) {
+	var vm = this;
+
+	var oFakeContact = {
+		name: "Andrey Savelev",
+		phone: "(555) 555-55-55",
+		email: "savelevcorr@gmail.com"
+	};
+
+
+	// Helper functions
 	function showToastMessage (sMessage) {
 		$mdToast.show(
 			$mdToast.simple()
@@ -22,83 +32,86 @@ function ClassifiedController($scope, $http, $mdSidenav, $mdToast, classifiedFac
 		return _.uniq(categories);
 	}
 
-	var oFakeContact = {
-		name: "Andrey Savelev",
-		phone: "(555) 555-55-55",
-		email: "savelevcorr@gmail.com"
-	};
+	// Main Logic
+	function toggleSidenav() {
+		$mdSidenav('leftSidenav').toggle();
+	}
+
+	function openSidenav() {
+		$mdSidenav('leftSidenav').open();	
+	}
+
+	function closeSidenav() {
+		$mdSidenav('leftSidenav').close();
+	}
+
+	function saveClassified(classifiedItem) {
+		if (classifiedItem) {
+			classifiedItem.contact = oFakeContact;
+			vm.classifieds.push(classifiedItem);
+			// Clear form fields
+			vm.classified = {};
+			toggleSidenav();
+			showToastMessage('Classified ' + classifiedItem.title + ' saved!');
+		}
+	}
+
+	function editClassified(classified) {
+		vm.edititng = true;
+		vm.classified = classified;
+		
+		openSidenav();
+	}
+
+	function saveEditedClassified() {
+		vm.edititng = false;
+		// Clear form fields
+		vm.classified = {};
+		closeSidenav();
+
+		showToastMessage('Edited classified saved');
+	}
+
+	function deleteClassified(classified) {
+		var nIndex = vm.classifieds.indexOf(classified);
+		vm.classifieds.splice(nIndex, 1);
+	}
+
+	function toggleFilters() {
+		vm.showFilters = !vm.showFilters;
+	}
+
+	function clearFilters() {
+		vm.classifiedsFilter = '';
+		vm.category = '';
+	}
 
 	classifiedFactory
 		.getClassifieds()
 		.then(function (responce) {
-			$scope.classifieds = responce.data;
-			$scope.categories = getCategories($scope.classifieds);
+			vm.classifieds = responce.data;
+			vm.categories = getCategories(vm.classifieds);
 		})
 		.catch(function (error) {
 			console.error(error.message);
 		});
 
-	$scope.showFilters = false;
 
-	$scope.toggleSidenav = function () {
-		$mdSidenav('leftSidenav').toggle();
-	};
+	// Public API
 
-	$scope.openSidenav = function () {
-		$mdSidenav('leftSidenav').open();	
-	};
+	// Methods
+	vm.toggleSidenav = toggleSidenav;
+	vm.openSidenav = openSidenav;
+	vm.closeSidenav = closeSidenav;
+	vm.saveClassified = saveClassified;
+	vm.editClassified = editClassified;
+	vm.saveEditedClassified = saveEditedClassified;
+	vm.deleteClassified = deleteClassified;
+	vm.toggleFilters = toggleFilters;
+	vm.clearFilters = clearFilters;
 
-	$scope.closeSidenav = function () {
-		$mdSidenav('leftSidenav').close();
-	};
-
-	// Add new classified item
-	$scope.saveClassified = function (classifiedItem) {
-		if (classifiedItem) {
-			classifiedItem.contact = oFakeContact;
-			$scope.classifieds.push(classifiedItem);
-			// Clear form fields
-			$scope.classified = {};
-			$scope.toggleSidenav();
-			showToastMessage('Classified ' + classifiedItem.title + ' saved!');
-		}
-
-	};
-
-	// Edit existing classified item
-	$scope.editClassified = function (classified) {
-		$scope.edititng = true;
-		$scope.classified = classified;
-		
-		$scope.openSidenav();
-	};
-
-	// Save edited classified item
-	$scope.saveEditedClassified = function () {
-		$scope.edititng = false;
-		// Clear form fields
-		$scope.classified = {};
-		$scope.closeSidenav();
-
-		showToastMessage('Edited classified saved');
-	};
-
-	// Delete classified item
-	$scope.deleteClassified = function (classified) {
-		var nIndex = $scope.classifieds.indexOf(classified);
-		$scope.classifieds.splice(nIndex, 1);
-	};
-
-	// Show/Hide Filters 
-	$scope.toggleFilters = function () {
-		$scope.showFilters = !$scope.showFilters;
-	};
-
-	// Clear all filters
-	$scope.clearFilters = function () {
-		$scope.classifiedsFilter = '';
-		$scope.category = '';
-	};
+	// Properties
+	vm.showFilters = false;
 }
 
 module.exports = ClassifiedController;
