226012f64829250fd55aaf0a5a1edca76057bb46
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-05 14:26:31 +0300

    set up ui-routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var materialConfig = require('./app/config/ng.config');
 var classifiedFactory = require('./app/factories/classifieds.factory');
 
 require('angular-material');
+require('angular-ui-router');
 require('angular-material/angular-material.css');
 require('mdi/css/materialdesignicons.min.css');
 require('./index.css');
@@ -11,7 +12,7 @@ require('./index.css');
 const CLASSIFIED = 'classified';
 
 angular
-  .module(CLASSIFIED, ['ngMaterial'])
+  .module(CLASSIFIED, ['ngMaterial', 'ui.router'])
   .config(materialConfig)
   .factory('classifiedFactory', classifiedFactory)
   .controller('ClassifiedController', ClassifiedController);
